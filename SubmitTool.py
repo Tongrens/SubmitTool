7245320943d47aa71619e71b803d00e56dc5f2cc
Tongrens <73508047+Tongrens@users.noreply.github.com>
2023-04-11 14:08:39 +0800

    update
    1. 更新登录方式
    2. 不再生成img文件

--- SubmitTool.py
+++ SubmitTool.py
@@ -1,40 +1,35 @@
-import os
 import time
 import json
 import base64
 import requests
+from PIL import Image
+from io import BytesIO
 
 
 class SubmitTool:
     def __init__(self, eid, access_token):
         self.extra_info, self.req_info, self.out_info = {}, [], ''
+        self.access_token, self.eid = access_token, eid
         self.main_url = 'https://api-xcx-qunsou.weiyoubot.cn'
-        self.access_token = access_token
-        self.eid = eid
+        self.get_user_info_url = f'{self.main_url}/xcx/enroll/v1/userinfo?access_token={self.access_token}'
 
     # 获取用户已保存数据(extra_info)
     def get_user_info(self):
-        get_user_info_url = self.main_url + '/xcx/enroll/v1/userinfo?access_token=' + self.access_token
-        user_info = json.loads(requests.get(get_user_info_url).text)
+        user_info = json.loads(requests.get(self.get_user_info_url).text)
         for i in user_info['data']['extra_info']:
             self.extra_info[i['name']] = i['value']
         print('当前已保存用户信息：')
         for i in self.extra_info:
-            print(i + '：' + self.extra_info[i])
+            print(f'{i}：{self.extra_info[i]}')
         if input('是否需要添加用户信息？(y/n)').lower() == 'y':
             print('请输入需要添加的信息(eg:\'姓名:张三\'),输入\'end\'结束')
-            while True:
-                tmp = input()
-                if tmp == 'end':
-                    break
-                else:
-                    if ':' in tmp:
-                        self.extra_info[tmp.split(':')[0]] = tmp.split(':')[1]
-                    else:
-                        print('输入格式错误')
+            while (tmp := input()) != 'end':
+                tmp = tmp if ':' in tmp else print('输入格式错误')
+                if tmp:
+                    self.extra_info[tmp.split(':')[0]] = tmp.split(':')[1]
             print('当前已保存用户信息：')
             for i in self.extra_info:
-                print(i + '：' + self.extra_info[i])
+                print(f'{i}：{self.extra_info[i]}')
 
     # 获取需要提交的数据并制作提交的字典(req_info)
     def get_info(self):
@@ -45,34 +40,26 @@ class SubmitTool:
             print('获取数据失败，将再次获取')
             return False
         for i in info['data']['req_info']:
-            if i['field_name'] in self.extra_info:
-                self.req_info.append({"field_name": i['field_name'], "field_value": self.extra_info[i['field_name']],
-                                      "field_key": i["field_key"]})
-            else:
-                self.out_info += i['field_name'] + '已提交为12345678910，请后续自行更改内容' + '\n'
-                tmp = '12345678910'
-                self.req_info.append({"field_name": i['field_name'], "field_value": tmp, "field_key": i["field_key"]})
-        if self.req_info:
-            return True
-        else:
-            return False
+            tmp = self.extra_info[i['field_name']] if i['field_name'] in self.extra_info else '12345678910'
+            self.req_info.append({"field_name": i['field_name'], "field_value": tmp,
+                                  "field_key": i["field_key"]})
+        return True if self.req_info else False
 
     # 提交数据(req_info)
     def post(self):
-        post_url = self.main_url + '/xcx/enroll/v5/enroll'
         body = {"access_token": self.access_token, "eid": self.eid, "info": self.req_info,
                 "on_behalf": 0, "items": [], "referer": "", "fee_type": ""}
-        return_info = json.loads(requests.post(post_url, json=body).text)
+        return_info = json.loads(requests.post(self.main_url + '/xcx/enroll/v5/enroll', json=body).text)
         if not return_info['sta']:  # 提交成功返回的sta为0，不成功为-1
             print(self.out_info[:-1])
             print(time.strftime("%H:%M:%S", time.localtime()) + '提交成功')
             return True
         else:
-            if return_info['msg'] == '提交次数超过限制':  # 超过限制为已提交，不需要再次运行
-                print('提交次数超过限制')
+            if return_info['msg'] == '活动期间，只允许提交1次':  # 超过限制为已提交，不需要再次运行
+                print('活动期间，只允许提交1次')
                 return True
             else:
-                print('提交失败，返回信息：' + return_info['msg'])
+                print(f"提交失败，返回信息：{return_info['msg']}")
                 return False
 
     # 类主入口
@@ -88,40 +75,53 @@ class SubmitTool:
 
 class GetToken:
     def __init__(self):
-        self.qr_url = 'https://api-xcx-qunsou.weiyoubot.cn/xcx/enroll_web/v1/pc_code'
-        self.login_url = 'https://api-xcx-qunsou.weiyoubot.cn/xcx/enroll_web/v1/pc_login?code='
+        self.get_qr_url = 'https://api-xcx-qunsou.weiyoubot.cn/xcx/enroll_web/v1/pc_code'
+        self.qr_login_url = 'https://api-xcx-qunsou.weiyoubot.cn/xcx/enroll_web/v1/pc_login?code='
+        self.phone_login_url = 'https://api-xcx-qunsou.weiyoubot.cn/xcx/enroll/v1/login_by_phone'
+        self.get_history_url = 'https://api-xcx-qunsou.weiyoubot.cn/xcx/enroll/v1/user/history?access_token='
 
     # 通过qr码方式登录
     def get_token_qr(self):
-        result = json.loads(requests.get(self.qr_url).text)  # 获取qr码及对应code
+        result = json.loads(requests.get(self.get_qr_url).text)  # 获取qr码及对应code
         code = result['data']['code']
-        img = result['data']['qrcode'][22:]
-        with open('qr.jpg', 'wb') as f:
-            f.write(base64.b64decode(img))  # 写入qr码以便打开图片
+        image = Image.open(BytesIO(base64.b64decode(result['data']['qrcode'][22:])))
         print('请使用微信扫码登录')
-        os.system('start qr.jpg')
+        image.show()
         while True:  # 循环判断登录是否成功，未登录sta为-1，登录成功为0并返回access_token
-            login_data = json.loads(requests.get(self.login_url + code).text)
+            login_data = json.loads(requests.get(self.qr_login_url + code).text)
             if not login_data['sta']:
                 return login_data['data']['access_token']
             print('等待登录...')
             time.sleep(1)
 
     # 通过手机号及密码方式登录
-    @staticmethod
-    def get_token_phone(phone, password):
-        url = 'https://api-xcx-qunsou.weiyoubot.cn/xcx/enroll/v1/login_by_phone'
+    def get_token_phone(self):
+        phone = input('请输入手机号：')
+        password = input('请输入密码：')
         data = {"phone": phone, "password": password}
-        return json.loads(requests.post(url, json=data).text)['data']['access_token']
+        res = json.loads(requests.post(self.phone_login_url, json=data).text)
+        if res['sta'] == -1:
+            print(f"登录失败，{res['msg']}")
+            return None
+        else:
+            return res['data']['access_token']
 
     # 类主入口
     def main(self):
         # 登录
-        # token = self.get_token_phone('phone', 'password')
-        token = self.get_token_qr()
+        while True:
+            login_type = input('请选择登录方式(1.二维码登录 2.手机号登录)：')
+            if login_type == '1':
+                token = self.get_token_qr()
+                break
+            if login_type == '2':
+                token = self.get_token_phone()
+                if not token:
+                    return
+                break
+            print('输入错误，请重新输入')
         # 获取个人历史记录
-        get_history_url = 'https://api-xcx-qunsou.weiyoubot.cn/xcx/enroll/v1/user/history?access_token=' + token
-        result = json.loads(requests.get(get_history_url).text)
+        result = json.loads(requests.get(self.get_history_url + token).text)
         history_data = []
         for i in result['data']:
             if i['status'] < 2:  # status状态码：0(未开始) 1(进行中) 2(已截至)
@@ -132,13 +132,11 @@ class GetToken:
         else:
             print('请选择需要提交的表单序号')
         for i in range(len(history_data)):
-            print('序号：' + str(i + 1) + '\t\t' + '名称：' + history_data[i]['name'] + '\t\t' + '状态：' + history_data[i][
-                'status'])
+            print(f"序号：{i + 1}\t\t名称：{history_data[i]['name']}\t\t状态：{history_data[i]['status']}")
         while True:
             user_input = input('请输入序号：')
             if user_input.isnumeric() and 0 < int(user_input) <= len(history_data):
-                run = SubmitTool(history_data[int(user_input) - 1]['eid'], token)  # 选择eid并进行提交
-                run.main()
+                SubmitTool(history_data[int(user_input) - 1]['eid'], token).main()  # 选择eid并进行提交
                 break
             else:
                 print('请输入正确的序号')
@@ -148,5 +146,4 @@ class GetToken:
 if __name__ == '__main__':
     main = GetToken()
     main.main()
-    os.remove('qr.jpg')
     input('按回车退出...')
